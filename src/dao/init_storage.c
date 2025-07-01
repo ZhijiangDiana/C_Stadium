de22f2ae310bf90a940764f85dc19e452b0136a6
嘉然今天吃向晚 <1483823709@qq.com>
2025-07-01 15:21:04 +0800

    实体的持久化

--- src/dao/init_storage.c
+++ src/dao/init_storage.c
@@ -4,17 +4,19 @@
 
 #include "init_storage.h"
 
+#define FIELD_DB_FILE          "data/field.db"
+#define RESV_INFO_DB_FILE      "data/resv.db"
+#define STUDENT_DB_FILE        "data/student.db"
 
 static list_t * field_list = NULL;
 static list_t * resv_info_list = NULL;
 static list_t * student_list = NULL;
 
 void init_storage() {
-    // TODO 读取所有文件，获取数据
-
-    field_list = init_list();
-    resv_info_list = init_list();
-    student_list = init_list();
+    // 读取所有文件，获取数据
+    load_from_file(field_list, FIELD_DB_FILE, field_t_serializer);
+    load_from_file(resv_info_list, RESV_INFO_DB_FILE, resv_info_t_serializer);
+    load_from_file(student_list, STUDENT_DB_FILE, student_t_serializer);
 }
 
 list_t* get_list(DB_TYPE type) {
@@ -31,5 +33,18 @@ list_t* get_list(DB_TYPE type) {
 }
 
 void store(DB_TYPE type) {
-    // TODO 根据需要存储的类型，持久化存储对应的数据
+    // 根据需要存储的类型，持久化存储对应的数据
+    switch (type) {
+        case FIELD_DB:
+        save_to_file(field_list, FIELD_DB_FILE, field_t_deserializer);
+        break;
+        case RESV_INFO_DB:
+        save_to_file(resv_info_list, RESV_INFO_DB_FILE, resv_info_t_deserializer);
+        break;
+        case STUDENT_DB:
+        save_to_file(student_list, STUDENT_DB_FILE, student_t_deserializer);
+        break;
+        default:
+        break;
+    }
 }
