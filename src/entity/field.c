9840fe2b4ec9f78eae50cff7f0cec99b49f0d1db
嘉然今天吃向晚 <1483823709@qq.com>
2025-07-01 19:31:38 +0800

    field_t序列化反序列化问题修复

--- src/entity/field.c
+++ src/entity/field.c
@@ -14,14 +14,19 @@ char* field_t_serializer(field_t* input) {
 
     // 临时缓冲区用于格式化时间
     char time_buffer[64];
-    format_time(&(input->open_from), time_buffer, sizeof(time_buffer));
+
+    // 格式化起始时间
     char open_from_str[64];
-    strncpy(open_from_str, time_buffer, sizeof(open_from_str) - 1);
+    sprintf(open_from_str, "%d-%d-%dT%d:%d",
+        input->open_from.year, input->open_from.month, input->open_from.day,
+        input->open_from.hour, input->open_from.minute);
     open_from_str[sizeof(open_from_str) - 1] = '\0';
 
-    format_time(&(input->open_to), time_buffer, sizeof(time_buffer));
+    // 格式化终止时间
     char open_to_str[64];
-    strncpy(open_to_str, time_buffer, sizeof(open_to_str) - 1);
+    sprintf(open_to_str, "%d-%d-%dT%d:%d",
+        input->open_to.year, input->open_to.month, input->open_to.day,
+        input->open_to.hour, input->open_to.minute);
     open_to_str[sizeof(open_to_str) - 1] = '\0';
 
     // 将各个字段拼接到缓冲区中
@@ -50,18 +55,18 @@ field_t* field_t_deserializer(char* input) {
     token = strtok(NULL, " ");
     strncpy(time_buffer, token, sizeof(time_buffer) - 1);  // open_from 时间
     time_buffer[sizeof(time_buffer) - 1] = '\0';
-    sscanf(time_buffer, "%d-%d-%d %d:%d",
-              &result->open_from.year, result->open_from.month, result->open_from.day,
-              result->open_from.hour, result->open_from.minute);
+    sscanf_s(time_buffer, "%d-%d-%dT%d:%d",
+              &result->open_from.year, &result->open_from.month, &result->open_from.day,
+              &result->open_from.hour, &result->open_from.minute);
     result->open_from.second = 0;
 
     token = strtok(NULL, " ");
     strncpy(time_buffer, token, sizeof(time_buffer) - 1);  // open_to 时间
     time_buffer[sizeof(time_buffer) - 1] = '\0';
-    sscanf(time_buffer, "%d-%d-%d %d:%d",
-          &result->open_from.year, result->open_from.month, result->open_from.day,
-          result->open_from.hour, result->open_from.minute);
-    result->open_from.second = 0;
+    sscanf_s(time_buffer, "%d-%d-%dT%d:%d",
+          &result->open_to.year, &result->open_to.month, &result->open_to.day,
+          &result->open_to.hour, &result->open_to.minute);
+    result->open_to.second = 0;
 
     token = strtok(NULL, " ");
     result->total_resv_cnt = atoi(token);  // 总预定次数
