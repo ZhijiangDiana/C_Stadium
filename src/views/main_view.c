31f0aae5da3cc2f3a0856a5b88934ec32bb12d31
嘉然今天吃向晚 <1483823709@qq.com>
2025-07-03 22:47:53 +0800

    主页菜单修改

--- src/views/main_view.c
+++ src/views/main_view.c
@@ -26,7 +26,6 @@ void enter_menu() {
         printf("1. 预约场地\n");
         printf("2. 查询场地预约情况\n");
         printf("3. 查看和取消预约\n");
-        printf("4. 录入学生信息\n");
         printf("请选择：");
 
         scanf_s("%d", &choice);
@@ -42,10 +41,6 @@ void enter_menu() {
             case 3:
                 // 进入取消预约界面
                 cancel_resv_view();
-                break;
-            case 4:
-                // 进入录入学生信息界面
-                student_create_menu();
                 break;
             case 114514:
                 admin_menu();
@@ -66,8 +61,9 @@ void admin_menu() {
 
         printf("==========欢迎使用工地通-羽毛球场预约系统==========\n");
         printf("1. 录入场地信息\n");
-        printf("2. 回到用户界面\n");
-        printf("3. 退出程序\n");
+        printf("2. 录入场地信息\n");
+        printf("3. 回到用户界面\n");
+        printf("4. 退出程序\n");
         printf("请选择：");
         scanf_s("%d", &choice);
         switch (choice) {
@@ -76,9 +72,12 @@ void admin_menu() {
             field_create_menu();
             break;
         case 2:
+            // 进入录入学生信息界面
+            student_create_menu();
+        case 3:
             // 返回用户界面
             return;
-        case 3:
+        case 4:
             printf("感谢使用！\n");
             store(ALL_DB);
             system("pause");
